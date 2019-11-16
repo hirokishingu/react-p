da420e759e4a2da14531e3a84db1605852380cb3
hirokishingu <hrk.bass.526@gmail.com>
2019-11-16 12:06:16 +0900

    Fix app.js..

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,10 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({ value: state.count.value })
-const mapDispatchToProps = dispatch => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement()),
-})
+//const mapDispatchToProps = dispatch => ({
+//  increment: () => dispatch(increment()),
+//  decrement: () => dispatch(decrement()),
+//})
+const mapDispatchToProps = ({ increment, decrement })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
