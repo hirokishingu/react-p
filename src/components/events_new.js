770667037cb8f7e5f06929829f442bae6a265487
hirokishingu <hrk.bass.526@gmail.com>
2019-11-16 13:36:38 +0900

    Add form validation..

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -26,7 +26,7 @@ class EventsNew extends Component {
   }
 
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
     return (
       <React.Fragment>
         <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -36,7 +36,7 @@ class EventsNew extends Component {
           </div>
 
           <div>
-            <input type="submit" value="Submit" disabled={false} />
+            <input type="submit" value="Submit" disabled={pristine || submitting} />
             <Link to="/">Cancel</Link>
           </div>
         </form>
