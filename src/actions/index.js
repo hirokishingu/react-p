3822a4b4eaaa06634d5bc820722d72005d373621
hirokishingu <hrk.bass.526@gmail.com>
2019-11-16 11:45:34 +0900

    Fix icement typo..

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-export const increment = () => {
+export const increment = () => ({
   type: INCREMENT
-}
+})
 
 export const decrement = () => ({
   type: DECREMENT
